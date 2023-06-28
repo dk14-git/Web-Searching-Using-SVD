942164f5625a9243140e17f8aa6ff55d19345815
Dhanush Krishna R <116013434+dk14-git@users.noreply.github.com>
2023-06-28 15:42:02 +0530

    Add files via upload

---
+++ scraper.m
@@ -0,0 +1,18 @@
+
+rowno=1996;
+i=1;
+
+while i<=rowno
+    try
+        k=webread(data(i,2));
+        p=extractHTMLText(k);
+        data(i,4)=p;
+        i=i+1
+    catch
+        data(i,4)=data(i,3);
+        i=i+1
+    end
+    
+    
+end
+    
